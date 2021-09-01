be39399cd345d05809fba65f401badf13fa200be
monicasalas86 <monicsalas167@gmail.com>
2021-08-31 23:52:33 -0500

    added 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+// mouseover
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach(navLink => navLink.addEventListener('mouseover', function(event){
+    event.target.style.color = 'lightblue';
+    setTimeout(function(){
+        event.target.style.color = '';
+    }, 300);
+}, false));
+
+// wheel
+const header = document.querySelector('header')
+
+header.addEventListener('wheel', function(){
+    header.style.background = 'lightgray';
+})
+
+// keyup
+const body = document.querySelector('body');
+
+body.addEventListener('keyup', function(){
+    body.style.background = 'thistle';
+})
